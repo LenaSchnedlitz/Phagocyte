63ecba6f00e2cec21891e8cc8360dbaacc3f652f
Lena Schnedlitz <git.lena.schnedlitz@web.de>
2020-03-31 21:07:51 +0200

    Refine eat implementation

--- eat.js
+++ eat.js
@@ -1,12 +1,18 @@
 const keywords = [
+  'beatmungsgerät',
   'corona',
+  'cov-19',
   'covid-19',
   'flatten the curve',
   'flattenthecurve',
+  'infiziert',
+  'kontaktverbot',
+  'maskenpflicht',
   'outbreak',
   'pandemi',
   'quarant',
   'sars-cov-2',
+  'schutzmaske',
   'self isolation',
   'selbstisolation',
   'seuche',
@@ -27,16 +33,30 @@ function eat(collection) {
   });
 }
 
-eat(document.getElementsByTagName('article'));
+function eatEverything() {
+  eat(document.getElementsByTagName('article'));
 
-eat(document.getElementsByClassName('athing'));
-eat(document.getElementsByClassName('block__position'));
-eat(document.getElementsByClassName('card'));
-eat(document.getElementsByClassName('linkBar'));
-eat(document.getElementsByClassName('multiTeaser-card'));
-eat(document.getElementsByClassName('pB-kurierat'));
-eat(document.getElementsByClassName('Post'));
-eat(document.getElementsByClassName('Post-item'));
-eat(document.getElementsByClassName('readmore__item'));
-eat(document.getElementsByClassName('upost'));
-eat(document.getElementsByClassName('widget-type-calltoaction'));
+  eat(document.getElementsByClassName('article-preview'));
+  eat(document.getElementsByClassName('athing'));
+  eat(document.getElementsByClassName('block__position'));
+  eat(document.getElementsByClassName('card'));
+  eat(document.getElementsByClassName('linkBar'));
+  eat(document.getElementsByClassName('multiTeaser-card'));
+  eat(document.getElementsByClassName('pB-kurierat'));
+  eat(document.getElementsByClassName('Post'));
+  eat(document.getElementsByClassName('Post-item'));
+  eat(document.getElementsByClassName('readmore__item'));
+  eat(document.getElementsByClassName('upost'));
+  eat(document.getElementsByClassName('vodl-lead-hero'));
+  eat(document.getElementsByClassName('vodl-news-aside__item'));
+  eat(document.getElementsByClassName('vodl-news-river__item'));
+  eat(document.getElementsByClassName('vodl-newsslider__category'));
+  eat(document.getElementsByClassName('vodl-newsslider__list-item'));
+  eat(document.getElementsByClassName('widget-type-calltoaction'));
+}
+
+browser.storage.sync.get('enabled').then((res) => {
+  if (res.enabled) {
+    eatEverything();
+  }
+}).catch(() => eatEverything());

857c4c212df98824f6ab3833b1e74ba66259fb15
Lena Schnedlitz <git.lena.schnedlitz@web.de>
2020-04-01 22:25:47 +0200

    Extend website support

--- eat.js
+++ eat.js
@@ -1,5 +1,6 @@
 const keywords = [
   'beatmungsgerät',
+  'c-virus',
   'corona',
   'cov-19',
   'covid-19',
@@ -25,34 +26,52 @@ const keywords = [
 ];
 const regexp = `(${keywords.join('|')})`;
 
+const tagList = [
+    'article',
+    'iframe'
+];
+const classList = [
+  'article',
+  'article-preview',
+  'athing',
+  'block__position',
+  'card',
+  'cn__listitem',
+  'cover-spread__headline__wrapper',
+  'fc-item',
+  'gel-layout__item',
+  'ie-11_Fil(n)',
+  'linkBar',
+  'multiTeaser-card',
+  'pancake-item-1',
+  'pancake-item-2',
+  'pancake-item-3',
+  'pancake-item-4',
+  'pb-feature',
+  'pB-kurierat',
+  'Post',
+  'Post-item',
+  'readmore__item',
+  'upost',
+  'vodl-lead-hero',
+  'vodl-news-aside__item',
+  'vodl-news-river__item',
+  'vodl-newsslider__category',
+  'vodl-newsslider__list-item',
+  'widget-type-calltoaction'
+];
+
 function eat(collection) {
   [...collection].forEach(entry => {
-    if (entry.innerHTML.toLowerCase().match(regexp)) {
+    if (entry.outerHTML.toLowerCase().match(regexp)) {
       entry.style.display = 'none';
     }
   });
 }
 
 function eatEverything() {
-  eat(document.getElementsByTagName('article'));
-
-  eat(document.getElementsByClassName('article-preview'));
-  eat(document.getElementsByClassName('athing'));
-  eat(document.getElementsByClassName('block__position'));
-  eat(document.getElementsByClassName('card'));
-  eat(document.getElementsByClassName('linkBar'));
-  eat(document.getElementsByClassName('multiTeaser-card'));
-  eat(document.getElementsByClassName('pB-kurierat'));
-  eat(document.getElementsByClassName('Post'));
-  eat(document.getElementsByClassName('Post-item'));
-  eat(document.getElementsByClassName('readmore__item'));
-  eat(document.getElementsByClassName('upost'));
-  eat(document.getElementsByClassName('vodl-lead-hero'));
-  eat(document.getElementsByClassName('vodl-news-aside__item'));
-  eat(document.getElementsByClassName('vodl-news-river__item'));
-  eat(document.getElementsByClassName('vodl-newsslider__category'));
-  eat(document.getElementsByClassName('vodl-newsslider__list-item'));
-  eat(document.getElementsByClassName('widget-type-calltoaction'));
+  tagList.forEach(entry => eat(document.getElementsByTagName(entry)));
+  classList.forEach(entry => eat(document.getElementsByClassName(entry)));
 }
 
 browser.storage.sync.get('disabled').then((res) => {
